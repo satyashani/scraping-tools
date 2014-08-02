e1ca1300c3e6464a1503e63b78c530cb931d78f6
satyashani <satyashani@gmail.com>
2014-08-02 17:07:56 +0530

    Updates for url collection bug

--- googlelist/gsearch.js
+++ googlelist/gsearch.js
@@ -94,8 +94,8 @@ var handler = function(req,res,server){
         var res = [];
         $("div#ires li.g").each(function(){
             var u = $(this).find("h3.r a").attr("href");
-            var match = u.match(/http[s]*:[^&]*/);
-            res.push(match?match[0]:u);
+            var match = u.match(/\/url\?q=(http[s]*[^&]*)/);
+            res.push(match?decodeURIComponent(match[1]):u);
         });
         var tds = $("div#foot td"),hasMore = !!tds.size();
         tds.each(function(i){
