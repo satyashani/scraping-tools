3828eb08103f574e71bcae0aeb7f6231548d0dba
satyashani <satyashani@gmail.com>
2014-12-16 18:13:37 +0530

    DBC incorrect captcha reporting capability

--- dmca/gserver.js
+++ dmca/gserver.js
@@ -302,128 +302,161 @@ var countrycodes = [
     {name: "Unlisted", code: "ZZ"}
 ]
 
+
 var  captchaApis = {
-    "dbc": function(captchafile,callback){
-        var dbc = {username: "hugs",password: "ugosara"};
-        var page = pg.create(),urlchanges= 0,timeout=false;
-        page.content = "<html><body>" +
-            "<form action='http://api.dbcapi.me/api/captcha' method='post' enctype='multipart/form-data'>" +
-            "<input type='text'     name='username' value='"+dbc.username+"'>"+
-            "<input type='password' name='password' value='"+dbc.password+"'>"+
-            "<input id='captchafile' type='file'     name='captchafile' >"+
-            "</form></body></html>";
-        page.injectJs(jq);
-        page.uploadFile("input#captchafile",captchafile);
-        page.onConsoleMessage = logger.log;
-        setTimeout(function(){
-            timeout=true;
-            callback(new Error("captcha_timeout"));
-        },conf.captchatimeout);
-        page.onUrlChanged = function(url){
-            urlchanges++;
-            logger.log("captcha_decode_step:url="+url);
-            if(url.match(/api\/captcha\/\d+/)){
-                page.injectJs(jq);
-                logger.log("captcha_decode_step:captcha uploaded");
-                var captcha = page.evaluate(function(captchaurl){
-                    var c = "",tries=0;
-                    var poll = function(){
-                        tries++;
-                        console.log("captcha_decode_step:poll try - "+tries);
-                        for(i=0,j=0;i<400000000;i++)
-                            j++;
-                        $.ajax({
-                            url: captchaurl,
-                            async: false,
-                            headers: {"Accept": "application/json"},
-                            success: function(data){
-                                console.log("captcha_decode_step:captcha decoded - "+tries);
-                                if(data.text)
-                                    c = data;
-                                else if(tries < 5){
-                                    poll();
-                                }
-                            },
-                            error: function(x,t,s){
-                                c = new Error(t);
-                                console.log("captcha_decode_step:error from DBC page - "+t);
-                                if(tries < 5){
-                                    poll();
-                                }
-                            }
-                        })
-                    }
-                    poll();
-                    return c;
-                },url);
-                if(timeout) return;
-                if(captcha.text)
-                    callback(null,captcha.text);
-                else
-                    callback(new Error("dbc_api_error:"+JSON.stringify(captcha)),null);
-            }else if(urlchanges==2 && !timeout)
-                callback(new Error("dbc_api_redirect_failed:"+url),null);
-        }
-        page.evaluate(function(){
-            $("form").submit();
-        });
-    },
-    "bpc": function(captchafile,callback){
-        var bpckey = "e5740194374a84031346a48321b32e58";
-        var page = pg.create(),urlchanges= 0,timeout=false;
-        var captcha = fs.read(captchafile).replace("base64:","");
-        page.content = "<html><body>" +
-            "<form id='uploadform' action='http://bypasscaptcha.com/upload.php' method='post' enctype='multipart/form-data'>"+
-            "<input type='text' name='key' value='"+bpckey+"'>"+
-            "<textarea name='file' id='captchafile'>"+captcha+"</textarea>"+
-            "<input type='text' name='gen_task_id' value='1'>"+
-            "<input type='text' name='base64_code' value='1'>"+
-            "<input type='text' name='vendor_key'>"+
-            "<input type='submit' value='Submit'>"+
-            "</form></body></html>";
-        page.injectJs(jq);
-        page.onConsoleMessage = logger.log;
-        setTimeout(function(){
-            timeout=true;
-            callback(new Error("captcha_timeout"));
-        },conf.captchatimeout);
-        page.onLoadFinished = function(){
-            urlchanges++;
+    "dbc": {
+        reqlist: {},
+        creds : {username: "hugs", password: "ugosara"},
+        solve: function (captchafile, callback) {
+            var dbc = captchaApis.dbc.creds;
+            var page = pg.create(), urlchanges = 0, calledBack = false;
+            page.content = "<html><body>" +
+                "<form action='http://api.dbcapi.me/api/captcha' method='post' enctype='multipart/form-data'>" +
+                "<input type='text'     name='username' value='" + dbc.username + "'>" +
+                "<input type='password' name='password' value='" + dbc.password + "'>" +
+                "<input id='captchafile' type='file'     name='captchafile' >" +
+                "</form></body></html>";
             page.injectJs(jq);
-            var hasform = page.evaluate(function(){
-                return $("form#uploadform").size();
-            });
-            if(hasform){
-                if(urlchanges <= 2)
-                    return;
-                else
-                    callback(new Error("error submitting captcha form"));
-            }
-            logger.log("captcha_decode_step:url="+page.url);
-            var content = page.evaluate(function(){
-                return $("body").text();
-            });
-            var lines = content.split("\n");
-            var  res = {};
-            lines.forEach(function(l){
-                var m = l.match(/([A-z]+) (.*)/);
-                if(m){
-                    res[m[1].toLowerCase()] = m[2];
+            page.uploadFile("input#captchafile", captchafile);
+            page.onConsoleMessage = logger.log;
+            var caller = function (err, res) {
+                if (!calledBack) {
+                    calledBack = true;
+                    callback(err, res);
                 }
+            }
+            setTimeout(function () {
+                caller(new Error("captcha_timeout"));
+            }, conf.captchatimeout);
+            page.onUrlChanged = function (url) {
+                urlchanges++;
+                logger.log("captcha_decode_step:url=" + url);
+                var m = url.match(new RegExp("api\/captcha\/([0-9]+)"));
+                if (m) {
+                    var id = m[1];
+                    page.injectJs(jq);
+                    logger.log("captcha_decode_step:captcha uploaded");
+                    var captcha = page.evaluate(function (captchaurl) {
+                        var c = "", tries = 0;
+                        var poll = function () {
+                            tries++;
+                            console.log("captcha_decode_step:poll try - " + tries);
+                            for (i = 0, j = 0; i < 400000000; i++)
+                                j++;
+                            $.ajax({
+                                url: captchaurl,
+                                async: false,
+                                headers: {"Accept": "application/json"},
+                                success: function (data) {
+                                    console.log("captcha_decode_step:captcha decoded - " + tries);
+                                    if (data.text) {
+                                        c = data;
+                                        captchaApis.dbc.reqlist[captchafile] = id;
+                                    }
+                                    else if (tries < 5) {
+                                        poll();
+                                    }
+                                },
+                                error: function (x, t, s) {
+                                    c = new Error(t);
+                                    console.log("captcha_decode_step:error from DBC page - " + t);
+                                    if (tries < 5) {
+                                        poll();
+                                    }
+                                }
+                            })
+                        }
+                        poll();
+                        return c;
+                    }, url);
+                    if (captcha.text)
+                        caller(null, captcha.text);
+                    else
+                        caller(new Error("dbc_api_error:" + JSON.stringify(captcha)), null);
+                } else if (urlchanges >= 2)
+                    caller(new Error("dbc_api_redirect_failed:" + url), null);
+            }
+            page.evaluate(function () {
+                $("form").submit();
             });
-            if(res.error) callback(new Error(res.error));
-            else if(!res.value) callback(new Error("No value in captcha response."));
-            else callback(null,res.value);
+        },
+        report: function(filename){
+            var page = pg.create();
+            var post = "username="+ captchaApis.dbc.creds.username+"&password="+ captchaApis.dbc.creds.password;
+            page.open("http://api.dbcapi.me/api/captcha/"+captchaApis.dbc.reqlist[filename]+"/report",'POST',post,function(stat){
+                if(stat != 'success') console.error("Failed to send captcha error request");
+            });
+        },
+        remove: function(filename){
+            fs.unlink(filename);
+            if(captchaApis.dbc.reqlist.hasOwnProperty(filename)) delete captchaApis.dbc.reqlist[filename];
         }
-        page.evaluate(function(){
-            $("form").submit();
-        });
+    },
+    "bpc": {
+        solve:
+            function(captchafile,callback){
+                var bpckey = "e5740194374a84031346a48321b32e58";
+                var page = pg.create(),urlchanges= 0,timeout=false;
+                var captcha = fs.read(captchafile).replace("base64:","");
+                page.content = "<html><body>" +
+                    "<form id='uploadform' action='http://bypasscaptcha.com/upload.php' method='post' enctype='multipart/form-data'>"+
+                    "<input type='text' name='key' value='"+bpckey+"'>"+
+                    "<textarea name='file' id='captchafile'>"+captcha+"</textarea>"+
+                    "<input type='text' name='gen_task_id' value='1'>"+
+                    "<input type='text' name='base64_code' value='1'>"+
+                    "<input type='text' name='vendor_key'>"+
+                    "<input type='submit' value='Submit'>"+
+                    "</form></body></html>";
+                page.injectJs(jq);
+                page.onConsoleMessage = logger.log;
+                setTimeout(function(){
+                    timeout=true;
+                    callback(new Error("captcha_timeout"));
+                },conf.captchatimeout);
+                page.onLoadFinished = function(){
+                    urlchanges++;
+                    page.injectJs(jq);
+                    var hasform = page.evaluate(function(){
+                        return $("form#uploadform").size();
+                    });
+                    if(hasform){
+                        if(urlchanges <= 2)
+                            return;
+                        else
+                            callback(new Error("error submitting captcha form"));
+                    }
+                    logger.log("captcha_decode_step:url="+page.url);
+                    var content = page.evaluate(function(){
+                        return $("body").text();
+                    });
+                    var lines = content.split("\n");
+                    var  res = {};
+                    lines.forEach(function(l){
+                        var m = l.match(/([A-z]+) (.*)/);
+                        if(m){
+                            res[m[1].toLowerCase()] = m[2];
+                        }
+                    });
+                    if(res.error) callback(new Error(res.error));
+                    else if(!res.value) callback(new Error("No value in captcha response."));
+                    else callback(null,res.value);
+                }
+                page.evaluate(function(){
+                    $("form").submit();
+                });
+            },
+        report: function(filename){
+
+        },
+        remove: function(filename){
+            fs.unlink(filename);
+        }
     }
 }
 
 var decodeCatpcha = function(captchafile,callback){
     if(conf.captchaApi && captchaApis.hasOwnProperty(conf.captchaApi)){
-        captchaApis[conf.captchaApi](captchafile,callback);
+        captchaApis[conf.captchaApi].solve(captchafile,callback);
     }else{
         callback(new Error("Unknown captcha api:"+conf.captchaApi));
     }
@@ -594,7 +627,6 @@ var worker = function(config){
             fs.write(filename,imgdata,"w");
             logger.log("retrieved captcha and wrote to file "+filename);
             decodeCatpcha(filename,function(err,decodedcaptcha){
-                fs.remove(filename);
                 var dmcaurlchanges = 0,dmcapageloads = 0;
                 if(err) return callback(err,null);
                 logger.log("captcha decoded to be "+decodedcaptcha);
@@ -621,6 +653,13 @@ var worker = function(config){
                         });
                         page.onLoadFinished = null;
                         if(errors){
+                            var time = new Date().toString().replace(/ /g,'_');
+                            page.render("aftersubmit_error"+time+".png");
+                            if(errors.indexOf("Incorrect response. Please try again")>-1 && captchaApis.hasOwnProperty(conf.captchaApi)){
+                                captchaApis[conf.captchaApi].report(filename);
+                            }else{
+                                captchaApis[conf.captchaApi].remove(filename);
+                            }
                             callback(new Error(errors),null);
                         }
                         else
